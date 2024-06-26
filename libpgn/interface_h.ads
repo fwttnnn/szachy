d7029bf8dd57b833b57905669a4423a3f5193fca
youkwhd <lolywk@tutanota.com>
2024-06-26 09:17:09 +0700

    chore: whoops

--- libpgn/interface_h.ads
+++ libpgn/interface_h.ads
@@ -4,7 +4,7 @@ pragma Style_Checks (Off);
 pragma Warnings (Off, "-gnatwu");
 
 with Interfaces.C; use Interfaces.C;
-limited with moves;
+limited with pgn_moves_h;
 
 package interface_h is
    procedure Moves_Foreach (moves : access pgn_moves_h.pgn_moves_t; callback : access procedure (arg1 : access pgn_moves_h.pgn_move_t))  -- interface.h:6
