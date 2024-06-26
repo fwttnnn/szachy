3f4e8b3af37593b95bebf274b5738468ced70065
youkwhd <lolywk@tutanota.com>
2024-06-26 10:16:15 +0700

    chore: add board, update pgn

--- szachy.adb
+++ szachy.adb
@@ -9,20 +9,26 @@ with Interfaces.C.Strings; use Interfaces.C.Strings;
 
 procedure Szachy is
     Pgn : access pgn_t;
-    Move : access uu_pgn_moves_item_t;
-
+    Pgn_Move : access uu_pgn_moves_item_t;
+    Board : array (1 .. 8, 1 .. 8) of Char := (
+        ( 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' ),
+        ( 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' ),
+        ( ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ),
+        ( ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ),
+        ( ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ),
+        ( ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ),
+        ( 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' ),
+        ( 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' )
+    );
 begin
     Pgn := Pgn_Init;
-    Pgn_Parse (Pgn, Interfaces.C.Strings.New_String ("[Result ""1/0""]" & LF & "1. ex4 e5 2. Nf3 Nc6 3. Bb5"));
+    Pgn_Parse (Pgn, Interfaces.C.Strings.New_String ("[White ""Fischer, Robert J.""]" & LF & "[Black ""Spassky, Boris V.""]" & LF & "1. e4 e5 2. Nf3 Nc6 3. Bb5 3... a6 4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6 8. c3 O-O 9. h3 Nb8 10. d4 Nbd7 11. c4 c6 12. cxb5 axb5 13. Nc3 Bb7 14. Bg5 b4 15. Nb1 h6 16. Bh4 c5 17. dxe5 Nxe4 18. Bxe7 Qxe7 19. exd6 Qf6 20. Nbd2 Nxd6 21. Nc4 Nxc4 22. Bxc4 Nb6 23. Ne5 Rae8 24. Bxf7+ Rxf7 25. Nxf7 Rxe1+ 26. Qxe1 Kxf7 27. Qe3 Qg5 28. Qxg5 hxg5 29. b3 Ke6 30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33. f3 Bc8 34. Kf2 Bf5 35. Ra7 g6 36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5 40. Rd6 Kc5 41. Ra6 Nf2 42. g4 Bd3 43. Re6 1/2-1/2"));
 
     for I in 0 .. Pgn.moves.length - 1 loop
-        Move := Moves_Access_Nth (Pgn.Moves, I);
-        Put_Line (Interfaces.C.To_Ada (Move.White.Notation));
-        Put_Line (Interfaces.C.To_Ada (Move.Black.Notation));
-        Put (LF);
+        Pgn_Move := Moves_Access_Nth (Pgn.Moves, I);
+        Put_Line (Interfaces.C.To_Ada (Pgn_Move.White.Notation));
+        Put_Line (Interfaces.C.To_Ada (Pgn_Move.Black.Notation));
     end loop;
-
-    Put_Line ("Hello, from Ada.");
 
     Pgn_Cleanup (Pgn);
 end Szachy;
